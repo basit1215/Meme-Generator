af86c88384b94d20468b37288f8257f554a1d33f
malik1243 <154369335+basit1215@users.noreply.github.com>
2024-10-08 23:28:01 +0500

    updated

---
+++ app/createMeme/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const createMeme = () => {
+  return (
+    <div>createMeme</div>
+  )
+}
+
+export default createMeme
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,66 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link';
 
-const Meme = () => {
-  return (
-    <h1>Meme</h1>
-  )
+interface Memes {
+    name: string;
+    url: string;
+    id: string
+}
+
+const Meme = async() => {
+
+    // const [memes, setMemes] = useState<any>([])
+
+    // useEffect(() => {
+
+    //     axios('https://api.imgflip.com/get_memes')
+    //         .then((res) => {
+    //             console.log(res.data.data.memes)
+    //             setMemes(res.data.data.memes)
+    //         })
+    //         .catch((err) => {
+    //             console.log(err)
+    //         })
+
+    // }, [])
+
+
+
+
+    const data = await fetch('https://api.imgflip.com/get_memes')
+    const res = await data.json()
+    console.log(res.data);
+    const meme = await res.data.memes
+    console.log(meme);
+
+    return (
+        <>
+
+            <h1>Meme</h1>
+            {meme.map((item: Memes) => (
+                <div key={item.id}>
+                    <Image
+                    src={item.url}
+                    alt={item.name}
+                    width={200}
+                    height={200}
+                    />
+                    <p>{item.name}</p>
+                    <Link href={{
+                        pathname: "createMeme/",
+                        query:{
+                            url: item.url,
+                            id: item.id,
+
+                        }
+                    }}>Generate Meme</Link>
+                </div>
+            ))
+            }
+
+        </>
+    )
 }
 
 export default Meme
