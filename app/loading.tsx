ed79dfccbcddaf9171757f7c6aaf148af15661bc
malik1243 <154369335+basit1215@users.noreply.github.com>
2024-10-08 19:51:33 +0500

    first commit

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <h1 className='text-center font-mono font-bold text-3xl mt-10'>Loading...</h1>
+  )
+}
+
+export default loading
---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <h1  className='text-center font-mono font-bold text-3xl mt-10'>Not Found!</h1>
+  )
+}
+
+export default NotFound
