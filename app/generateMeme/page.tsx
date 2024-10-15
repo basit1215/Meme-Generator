8ebda484484fca0e453ac65e3e06b3bf5d939fc5
malik1243 <154369335+basit1215@users.noreply.github.com>
2024-10-16 03:41:32 +0500

    updated

--- app/generateMeme/page.tsx
+++ app/generateMeme/page.tsx
@@ -1,197 +1,108 @@
-"use client"
+"use client";
 
-import React, { useRef, useState } from 'react'
 import Image from 'next/image';
+import React, { useState } from 'react';
 
-const memeGenerate = ({ searchParams }: { searchParams: { id: string; urls: string; box_count: number; } }) => {
+interface MemeProps {
+    name: string;
+    id: string;
+    urls: string;
+    box_count: number;
+}
 
-    const [meme, setMeme] = useState<string | null>(null)
-    const inp1 = useRef<HTMLInputElement>(null)
-    const inp2 = useRef<HTMLInputElement>(null)
-    const inp3 = useRef<HTMLInputElement>(null)
-    const inp4 = useRef<HTMLInputElement>(null)
+const CreateMeme = ({ searchParams }: { searchParams: MemeProps }) => {
+    const [generatedMemeImage, setGeneratedMemeImage] = useState<string | null>(null);
+    const [inputTexts, setInputTexts] = useState<{ [key: string]: string }>({});
+    const [loading, setLoading] = useState(false);
 
+    const handleInputChange = (index: number, value: string) => {
+        setInputTexts(prev => ({ ...prev, [`text_${index}`]: value }));
+    };
 
-    const generateMemes = async (e: React.FormEvent<HTMLFormElement>) => {
+    const generateMeme = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setLoading(true);
 
-        e.preventDefault()
-        console.log(inp1.current?.value)
-        console.log(inp2.current?.value)
+        const params = Object.keys(inputTexts)
+            .map((key, i) => `boxes[${i}][text]=${encodeURIComponent(inputTexts[key])}`)
+            .join('&');
 
-        const data = await fetch(`https://api.imgflip.com/caption_image?template_id=${searchParams.id}&username=abasitkhan&password=basit1215&text0=${inp1.current?.value}&text1=${inp2.current?.value}`, { method: 'POST' })
-        const res = await data.json()
-        console.log(res)
-        setMeme(res.data.url)
-        console.log(meme)
+        const url = `https://api.imgflip.com/caption_image?template_id=${searchParams.id}&username=abasitkhan&password=basit1215&${params}`;
 
-    }
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+
+            if (data.success) {
+                setGeneratedMemeImage(data.data.url);
+                setInputTexts({});
+            } else {
+                alert('Error: ' + data.error_message);
+            }
+        } catch (error) {
+            alert('Error: ' + (error as Error).message);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     return (
-        <>
-            <h1 className="mt-8 text-3xl font-bold text-gray-800 text-center">
-                Generate Meme 😅
-            </h1>
-    
-            <div className="max-w-md mx-auto p-6 bg-white shadow-lg rounded-lg mt-6">
-    
+        <div className='bg-gray-900 min-h-screen p-8 flex justify-center gap-12 flex-wrap'>
+      
+            <div className="max-w-md  p-6 bg-gray-300 shadow-lg rounded-lg mt-6">
+                <h1 className="mt-6 text-3xl font-bold mb-10 text-gray-900 text-center">
+                    Create Your Meme 😅
+                </h1>
                 <div className="flex justify-center mb-6">
                     <Image
                         src={searchParams.urls}
-                        width={300}
+                        width={500}
                         height={300}
-                        alt='meme'
+                        alt="meme"
                         className="rounded-lg shadow-md object-contain"
                     />
                 </div>
-    
-                <form onSubmit={generateMemes} className="space-y-4">
-                    <input
-                        type="text"
-                        placeholder="Enter text 1"
-                        ref={inp1}
-                        className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    />
-                    <input
-                        type="text"
-                        placeholder="Enter text 2"
-                        ref={inp2}
-                        className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    />
+                <form onSubmit={generateMeme} className="space-y-4">
+                    {Array.from({ length: searchParams.box_count }).map((_, i) => (
+                        <input
+                            key={i}
+                            type="text"
+                            value={inputTexts[`text_${i}`] || ''}
+                            onChange={(e) => handleInputChange(i, e.target.value)}
+                            placeholder={`Meme text ${i + 1}`}
+                            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-600"
+                        />
+                    ))}
                     <button
                         type="submit"
-                        className="w-full bg-gradient-to-r from-blue-500 to-indigo-600 text-white font-bold py-2 px-4 rounded-lg hover:from-blue-400 hover:to-indigo-500 transition-all duration-300 transform hover:-translate-y-1"
+                        disabled={loading}
+                        className="w-full bg-gradient-to-r from-gray-700 to-slate-900 text-white font-bold py-2 px-4 rounded-lg hover:from-gray-600 hover:to-slate-800 transition-all duration-300 transform"
                     >
-                        Create Meme
+                        {loading ? 'Generating...' : 'Generate Meme'}
                     </button>
                 </form>
-    
-                <h1 className="mt-8 text-2xl font-semibold text-gray-800 text-center">
-                    Your Generated Meme 😂🤣
-                </h1>
-    
-                {meme ? (
+            </div>
+   
+            <div className="max-w-md  w-[488px] p-6 bg-gray-300 shadow-lg rounded-lg mt-6">
+                <h1 className="mt-6 text-3xl font-bold mb-10 text-gray-900 text-center">Your Generated Meme 😂</h1>
+                {generatedMemeImage ? (
                     <div className="flex justify-center mt-4">
                         <Image
-                            alt='meme'
-                            width={300}
+                            src={generatedMemeImage}
+                            alt="Generated Meme"
+                            width={500}
                             height={300}
-                            src={meme}
                             className="rounded-lg shadow-lg object-contain"
                         />
                     </div>
                 ) : (
-                    <h1 className="mt-4 text-xl font-semibold text-red-500 text-center">
+                    <h1 className="mt-4 text-xl font-semibold  text-red-600 text-center">
                         No Meme Generated...
                     </h1>
                 )}
             </div>
-        </>
+        </div>
     )
-  
-}
+    }
 
-export default memeGenerate
-
-
-
-
-
-
-
-
-
-
-
-
-// "use client"
-
-// import React, { useRef, useState } from 'react'
-// import Image from 'next/image';
-
-// const memeGenerate = ({ searchParams }: { searchParams: { id: string; urls: string; box_count: number; } }) => {
-
-//     const [meme, setMeme] = useState<string | null>(null)
-//     const inp1 = useRef<HTMLInputElement>(null)
-//     const inp2 = useRef<HTMLInputElement>(null)
-//     const inp3 = useRef<HTMLInputElement>(null)
-//     const inp4 = useRef<HTMLInputElement>(null)
-
-
-//     const generateMemes = async (e: React.FormEvent<HTMLFormElement>) => {
-//         e.preventDefault()
-//         console.log(inp1.current?.value)
-//         console.log(inp2.current?.value)
-//         console.log(inp3.current?.value)
-//         console.log(inp4.current?.value)
-
-//         const data = await fetch(`https://api.imgflip.com/caption_image?template_id=${searchParams.id}&username=abasitkhan&password=basit1215&text0=${inp1.current?.value}&text1=${inp2.current?.value}&text2=${inp3.current?.value}&text3=${inp4.current?.value}`, { method: 'POST' })
-//         const res = await data.json()
-//         console.log(res)
-//         setMeme(res.data.url)
-//         console.log(meme)
-
-//     }
-
-//     return (
-//         <>
-//             <div>
-//                 <Image
-//                     src={searchParams.urls}
-//                     width={300}
-//                     height={300}
-//                     alt='meme'
-//                 />
-//                 {/* <div>
-//                     {
-//                      if(searchParams.box_count === 2){
-//                         <form onSubmit={generateMemes}>
-//                             <input type="text" placeholder='Text 01' ref={inp1} />
-//                             <input type="text" placeholder='Text 02' ref={inp2} />
-//                             <button type='submit'>Generate</button>
-//                         </form>
-//                     }
-//                     }
-//                 </div> */}
-
-
-//                 <div>
-//                     <form onSubmit={generateMemes}>
-//                         {/* Show input fields based on box_count */}
-//                         {searchParams.box_count >= 1 && (
-//                             <input type="text" placeholder="Text 01" ref={inp1} />
-//                         )}
-//                         {searchParams.box_count >= 2 && (
-//                             <input type="text" placeholder="Text 02" ref={inp2} />
-//                         )}
-//                         {searchParams.box_count >= 3 && (
-//                             <input type="text" placeholder="Text 03" ref={inp3} />
-//                         )}
-//                         {searchParams.box_count >= 4 && (
-//                             <input type="text" placeholder="Text 04" ref={inp4}/>
-//                         )}
-//                         {/* Add more inputs if needed */}
-//                         <button type='submit'>Generate</button>
-//                     </form>
-//                 </div>
-
-
-
-//                 <h1>Yours Meme</h1>
-//                 {meme ? (
-//                     <div>
-//                         <Image
-//                             alt='meme'
-//                             width={300}
-//                             height={300}
-//                             src={meme}
-//                         />
-//                     </div>
-//                 ) : (<h1>No Meme Generate...</h1>)
-
-//                 }
-//             </div>
-//         </>
-//     )
-// }
-
-// export default memeGenerate
+export default CreateMeme
--- app/page.tsx
+++ app/page.tsx
@@ -17,39 +17,46 @@ const Meme = async () => {
     const meme = await res.data.memes
     console.log(meme);
 
-
     return (
         <>
-            <div className="container mx-auto p-4">
-                <div className="text-center mb-6">
-                    <h1 className="text-3xl font-bold text-gray-800">Memes 😂</h1>
-                </div>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                    {meme.map((item: Memes) => (
-                        <div key={item.id} className="border rounded-lg shadow-lg overflow-hidden">
-                            <Image
-                                src={item.url}
-                                alt="meme"
-                                width={200}
-                                height={200}
-                                className="w-full h-48 object-cover"
-                            />
-                            <div className="p-4">
-                                <p className="text-lg font-semibold text-gray-700">{item.name}</p>
-                                <Link href={{
-                                    pathname: "generateMeme",
-                                    query: {
-                                        urls: item.url,
-                                        id: item.id,
-                                        box_count: item.box_count
-                                    }
-                                }} className="mt-2 inline-block bg-gradient-to-r from-blue-500 to-indigo-600 text-white font-bold py-2 px-6 rounded-lg shadow-lg hover:from-blue-400 hover:to-indigo-500 hover:shadow-xl transition-all duration-300 transform hover:-translate-y-1">
-                                    Generate Meme
-                                </Link>
-                            </div>
+            <div className='bg-gray-900 min-h-screen'>
+                <div className=" container mx-auto p-4 pb-10 ">
+                    
+                    <div className="text-center mb-6">
+                        <h1 className="text-3xl font-bold text-gray-300 ">Memes 😂</h1>
+                    </div>
 
-                        </div>
-                    ))}
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 " >
+                        {meme.map((item: Memes) => (
+                            <div key={item.id} className="border rounded-lg shadow-lg overflow-hidden bg-gray-300">
+                                <Image
+                                    src={item.url}
+                                    alt="meme"
+                                    width={200}
+                                    height={200}
+                                    className="w-full h-48 object-cover"
+                                />
+                                <div className="p-4 text-center">
+                                    <div>
+                                        <p className="text-lg font-semibold text-gray-700">{item.name}</p>
+                                    </div>
+                                    <div className='flex items-end justify-end'>
+                                        <Link href={{
+                                            pathname: "generateMeme",
+                                            query: {
+                                                urls: item.url,
+                                                id: item.id,
+                                                box_count: item.box_count
+                                            }
+                                        }} className="mt-2 inline-block bg-gradient-to-r from-gray-700 to-slate-900 text-white font-bold py-2  w-full rounded-lg shadow-lg hover:from-gray-600 hover:to-slate-800 hover:shadow-xl transition-all duration-300 transform ">
+                                            Generate Meme
+                                        </Link>
+                                    </div>
+                                </div>
+
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </>
